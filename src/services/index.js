eb9b0d0ef9cd4b8523386ffd4e625ce216a0ca32
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-07-01 02:00:43 +0530

    added utils.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const CityService = require("./city-service");
+const FlightService = require("./flight-service");
 
 module.exports={
-   CityService:require('./city-service')
+   CityService:require('./city-service'),
+   FlightService:require('./flight-service')
 }
