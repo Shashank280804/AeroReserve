3a14bcad71cb5c9b0b222f5d005eab96184ec451
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-07-01 19:09:25 +0530

    Added Enums and Middlewares

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,27 @@
+const {ClientErrorsCodes}=require('../utils/error-codes')
+const validateCreateFlight = (req, res, next) => {
+  if (
+    !req.body.flightNumber ||
+    !req.body.airplaneId ||
+    !req.body.departureAirportId ||
+    !req.body.arrivalAirportId ||
+    !req.body.arrivalTime ||
+    !req.body.departureTime ||
+    !req.body.price
+  ) {
+    //if any of the body param is missing we will come inside
+    return res.status(ClientErrorsCodes.BAD_REQUEST).json({
+      data: {},
+      success: false,
+      message: "Invalid request body for create flight",
+      err: "Missing mandatory properties to create a flight",
+    });
+  }
+  next();
+
+};
+
+
+module.exports={
+    validateCreateFlight
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const CityController = require('../../controllers/city-controller');
-const FlightController=require('../../controllers/flight-controller');
-const AirportController=require('../../controllers/airport-controller')
+const express = require("express");
+const CityController = require("../../controllers/city-controller");
+const FlightController = require("../../controllers/flight-controller");
+const AirportController = require("../../controllers/airport-controller");
+const { FlightMiddlewares } = require("../../middlewares/index");
 
 const router = express.Router();
 
-router.post('/city', CityController.create);
-router.delete('/city/:id', CityController.destroy);
-router.get('/city/:id', CityController.get);
-router.patch('/city/:id', CityController.update);
-router.get('/city',CityController.getAll);
+router.post("/city", CityController.create);
+router.delete("/city/:id", CityController.destroy);
+router.get("/city/:id", CityController.get);
+router.patch("/city/:id", CityController.update);
+router.get("/city", CityController.getAll);
 
-router.post('/flights',FlightController.create);
-router.get('/flights',FlightController.getAll);
+router.post(
+  "/flights",
+  FlightMiddlewares.validateCreateFlight,
+  FlightController.create
+);
+router.get("/flights", FlightController.getAll);
 
-router.post('/airports',AirportController.create);
+router.post("/airports", AirportController.create);
 module.exports = router;
